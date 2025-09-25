806f86fbd4311615c5bb1008d83aba3a09016fb8
JoseNB1207 <jose.ninob@uniagustiniana.edu.co>
2025-09-25 21:02:21 +0000

    Commit 6

---
+++ js/pokemon.js
@@ -0,0 +1,23 @@
+async function Pokemon(id){
+   
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = await res.json()
+
+        var root = document.getElementById("root");
+        root.innerHTML =`
+        <section class="c-detalle">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png" alt="${data.name}" height="120" width="auto">
+        <p>${data.name}</p>
+        <p>${data.id}</p>
+        <p>Altura: ${data.height / 10} m / Peso: ${data.weight / 10} kg</p>
+        <p>hp: ${data.stats[0].base_stat}</p>
+        <p>Velocidad: ${data.stats[5].base_stat}</p>
+        <p>Ataque: ${data.stats[1].base_stat} Defensa: ${data.stats[2].base_stat}</p>
+        <p>Ataque Especial: ${data.stats[3].base_stat} Defensa Especial: ${data.stats[4].base_stat}</p>
+
+    </section>` ;
+}
+Pokemon(5)
+Pokemon(43)
+Pokemon(1025)
+Pokemon(1024)
